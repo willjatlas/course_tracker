983b04b6016088309e555438469830bb584ae00f
William Anderson <willjatlas@googlemail.com>
2021-02-12 15:58:59 +0000

    api data appearling in use state

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,33 @@
+import userEvent from '@testing-library/user-event';
+import { useEffect , useState} from 'react';
 import './App.css';
+import { getCourses } from './services/CTAppServices';
 
 function App() {
+
+  const [courses, setCourses] = useState([]);
+
+  // const course = courses.map((element) =>{
+  //   return <p>{element.name}</p>
+  // });
+
+  useEffect(()=>{
+    getCourses().then((courses) => {
+      console.log(courses);
+      setCourses(courses);
+    })
+  }, []);
+
   return (
-    
+
     <>
       <h1>Now I need to re-learn React!</h1>
+      {/* {course} */}
+      
     </>
     
   );
+
 }
 
 export default App;
--- client/src/services/CTAppServices.js
+++ client/src/services/CTAppServices.js
@@ -1,2 +1,7 @@
-const baseURL= "http://localhost:8080/"
+const baseURL = "http://localhost:8080/"
 
+// GET - get all courses.
+export const getCourses = () => {
+    return fetch(baseURL + "courses")
+        .then(res => res.json())
+};
